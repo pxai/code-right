c30745fb4c58aa0410cada43c7b48a24e2911df5
Pello Altadill <pello.altadill@gluckgames.com>
2019-09-16 19:14:19 +0200

    Changed script

--- scripts/md2pdf.sh
+++ scripts/md2pdf.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # run from the content
 TEXT=
-TEMPL=../scripts/
-DIST=../dist/
+TEMPL=./scripts/
+DIST=./dist/
+TITLE="Code Right"
 
 # This wraps lines and adds line number...
 #     --listings -H ${TEMPL}listings-setup.tex \
@@ -10,10 +11,10 @@ DIST=../dist/
 #FILES=${TEXT}prologue.md ${TEXT}level1.md ${TEXT}level7.md
 pandoc --template=${TEMPL}plantilla-kdp.latex \
     -V language=spanish -V lang=spanish \
-    -V author='Pello Xabier Altadill Izura' -V title='Handbook'\
+    -V author='Pello Xabier Altadill Izura' -V title='Code Right'\
     -V documentclass=book\
     -S --latex-engine=xelatex  \
-    -o ${DIST}handbook.pdf  \
+    -o ${DIST}code-right.pdf  \
     prologue.md \
     00.language.md \
     01.testing.md \
