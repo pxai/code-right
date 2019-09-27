da4980432b2e7b9c68719a69eed6999c3f473327
Pello Altadill <pello.altadill@gluckgames.com>
2019-09-27 19:53:24 +0200

    added spellchecker

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,32 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+  "plugins": [
+    "spellcheck"
+  ],
   rules: {
+    "spellcheck/spell-checker": [1,
+      {
+          "comments": true,
+          "strings": true,
+          "identifiers": true,
+          "lang": "en_US",
+          "skipWords": [
+              "dict",
+              "aff",
+              "hunspellchecker",
+              "hunspell",
+              "utils"
+          ],
+          "skipIfMatch": [
+              "http://[^s]*",
+              "^[-\\w]+\/[-\\w\\.]+$" //For MIME Types
+          ],
+          "skipWordIfMatch": [
+              "^foobar.*$" // words that begin with foobar will not be checked
+          ],
+          "minLength": 3
+       }
+   ]
   }
 }
